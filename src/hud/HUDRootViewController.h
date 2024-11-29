8478e25ed2bfc10fefa0b58c1d5f95eea733a621
dm <dm@dm.com>
2024-11-30 05:40:38 +0800

    调试.2

--- src/hud/HUDRootViewController.h
+++ src/hud/HUDRootViewController.h
@@ -7,6 +7,12 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)pauseLoopTimer;
 - (void)resumeLoopTimer;
 - (void)reloadUserDefaults;
+
+/**
+ * 获取截图时是否隐藏小部件的设置
+ * @return BOOL 是否在截图时隐藏小部件
+ */
+- (BOOL)hideWidgetsInScreenshot;
 @end
 
 NS_ASSUME_NONNULL_END
